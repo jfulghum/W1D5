043da3d8ca6d7473125119472395f0426033cfa1
Chris Lee <chrislee92@gmail.com>
2017-09-08 17:27:04 -0400

    Change Knights.rb

--- skeleton/lib/Knights.rb
+++ skeleton/lib/Knights.rb
@@ -1,12 +1,22 @@
 require_relative '00_tree_node.rb'
 
 class KnightPathFinder
-  attr_accessor :visited_positions
+  attr_accessor :visited_positions, :pos
+
   def initialize(pos)
     @pos = pos
     @visited_positions = [pos]
     @board = Array.new(8) do
       Array.new(8) { PolyTreeNode.new}
+    end
+  end
+
+  def fill_board
+    board.each_with_index do |array, i|
+      array.each_with_index do |el, j|
+
+      end
+    end
 
   end
 
@@ -20,14 +30,20 @@ class KnightPathFinder
 
   def self.valid_moves(pos)
     moves = []
-    moves - visited_positions
+    # moves - visited_positions
 
-      moves << [pos[0] -2,pos[1]-1]
-      moves << [pos[0] -2,pos[1]+1]
-      moves << [pos[0] -1,pos[1]-2]
-      moves << [pos[0] -1,pos[1]-2]
-      moves.concat(moves.map {|pos| pos.reverse})
-    end
+    moves << [pos[0] -2,pos[1]-1]
+    moves << [pos[0] -2,pos[1]+1]
+    moves << [pos[0] -1,pos[1]-2]
+    moves << [pos[0] -1,pos[1]-2]
+    moves.concat(moves.map {|pos| pos.reverse})
+
     moves
   end
+
+
 end
+
+
+knight1 = KnightPathFinder.new([0,0])
+p KnightPathFinder.valid_moves([0,0])
